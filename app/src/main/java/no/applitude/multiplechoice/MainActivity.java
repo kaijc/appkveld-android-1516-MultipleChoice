9495c7727ad874b2abe0b3af68cb3bcf345c172f
Roy Olav Purser <admin@royolav.net>
2016-01-20 19:24:12 +0100

    first workshop 2016

---
+++ app/src/main/java/no/applitude/multiplechoice/MainActivity.java
@@ -0,0 +1,14 @@
+package no.applitude.multiplechoice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Player p = new Player("Name", "mage");
+    }
+}
